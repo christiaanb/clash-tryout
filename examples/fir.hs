c200a068ded2a066b896eb3ddb27de64be89bc50
Christiaan Baaij <christiaan.baaij@gmail.com>
2011-12-08 16:32:40 +0100

    Add FIR filter example

---
+++ examples/fir.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE Arrows, TemplateHaskell #-}
+module FIR where
+
+import CLaSH
+
+type Word = Signed D16
+
+fir hs pxs x = (xT, xT ** hs)
+  where
+    xT       = x +>> pxs
+    as ** bs = vfoldl (+) 0 (vzipWith (*) as bs)
+
+topEntity = (fir hs) ^^^ initFir
+  where
+    hs      = $(vTH [2,3,-2,(8::Word)])
+    initFir = vcopyn d4 0
